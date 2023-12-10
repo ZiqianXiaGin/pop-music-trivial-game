dc1834086d1abcad3ea3879f97759bd710952c24
Ziqian Xia <zxia@bu.edu>
2023-12-10 18:56:44 -0500

    12/10 6:56pm

--- scripts/lyricsgame.js
+++ scripts/lyricsgame.js
@@ -57,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function() {
   let usedQuestions = [];
   let score = 0;
   let timerId;
-  let secondChance = false;
   
 
   function getRandomQuestion() {
